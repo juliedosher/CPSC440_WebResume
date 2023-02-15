3e60853282f0ee259f3178e98f5642d2bc67781c
juliedosher <juliegdosher@lewisu.edu>
2023-02-15 15:08:34 -0600

    added messages for 404 and 500 errors

--- index.js
+++ index.js
@@ -10,5 +10,20 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));     
 });
 
+// Custom 404 page.
+app.use((request, response) => {
+  response.type('text/plain')
+  response.status(404)
+  response.send('404 - Not Found')
+})
+
+// Custom 500 page.
+app.use((err, request, response, next) => {
+  console.error(err.message)
+  response.type('text/plain')
+  response.status(500)
+  response.send('500 - Server Error')
+})
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
