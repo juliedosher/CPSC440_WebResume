9263750771a33a2732bd1838d4e43648aec13b68
juliedosher <juliegdosher@lewisu.edu>
2023-02-15 17:41:22 -0600

    trying res.render to show html file

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080; 
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/static/about.html');   
-});
-
 app.use(express.static(path.join(__dirname, 'static')));   
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
