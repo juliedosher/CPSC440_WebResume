be39cd43c3b71d8aa35cd28fbad1d5cbeba75ad6
juliedosher <juliegdosher@lewisu.edu>
2023-02-15 15:01:54 -0600

    adjusted path name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'static')));    
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/static/index.html'));     
+  res.sendFile(path.join(__dirname, 'index.html'));     
 });
 
 app.listen(port);
