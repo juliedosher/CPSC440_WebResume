f91f24a4cb458b2265d02fe84729fb0bdb523842
juliedosher <juliegdosher@lewisu.edu>
2023-02-15 15:10:38 -0600

    changed how index.html is accessed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'static')));    
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));     
+  res.render('index');    
 });
 
 // Custom 404 page.
